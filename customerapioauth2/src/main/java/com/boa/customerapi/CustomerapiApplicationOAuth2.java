388dec752bed32347c369f3ba372e56efaa4d78d
eswaribala <parameswaribala@gmail.com>
2022-10-20 15:42:38 +0530

    updated

---
+++ customerapioauth2/src/main/java/com/boa/customerapi/CustomerapiApplicationOAuth2.java
@@ -0,0 +1,13 @@
+package com.boa.customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerapiApplicationOAuth2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerapiApplicationOAuth2.class, args);
+	}
+
+}
