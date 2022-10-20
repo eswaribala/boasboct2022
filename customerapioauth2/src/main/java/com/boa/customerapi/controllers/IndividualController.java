f323e42da84b29ac7236cc0eae7bd82acb9e3229
eswaribala <parameswaribala@gmail.com>
2022-10-20 15:50:31 +0530

    updated

--- customerapioauth2/src/main/java/com/boa/customerapi/controllers/IndividualController.java
+++ customerapioauth2/src/main/java/com/boa/customerapi/controllers/IndividualController.java
@@ -3,8 +3,15 @@ package com.boa.customerapi.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,8 +26,11 @@ import com.boa.customerapi.models.Individual;
 import com.boa.customerapi.services.IndividualService;
 import com.boa.customerapi.vos.ResponseWrapper;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/individuals")
+@Slf4j
 public class IndividualController {
     @Autowired 
 	private IndividualService individualService;
@@ -39,11 +49,20 @@ public class IndividualController {
     				.body(new ResponseWrapper("Individual Input Not Correct"));
     	}
     }
+    @PreAuthorize("hasAuthority('SCOPE_TEST')")
     @GetMapping({"/v1.0/"})
     public List<Individual> getIndividuals(){
-    	
+    	SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = context.getAuthentication();
+		log.info("Scopes: " + authentication.getAuthorities());
+		
     	return this.individualService.getAllIndividuals();
     }
+    
+
+	
+	
+
     
     @PutMapping({"/v1.0/{customerId}"})
     public ResponseEntity<ResponseWrapper> updateIndividual(@PathVariable("customerId") long customerId,
