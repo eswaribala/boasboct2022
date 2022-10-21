bab848fc578eb27b965e0bbb5f8e595c5d5a2a04
eswaribala <parameswaribala@gmail.com>
2022-10-21 16:57:53 +0530

    updated

---
+++ customerapitracing/src/main/java/com/boa/customerapi/CustomerapiApplicationTracing.java
@@ -0,0 +1,13 @@
+package com.boa.customerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerapiApplicationTracing {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerapiApplicationTracing.class, args);
+	}
+
+}
